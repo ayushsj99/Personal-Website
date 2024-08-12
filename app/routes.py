980877c2dfffc8c01b75fc9d65b3d2a6b5b8e13e
ayushsj99 <ayushsjadhav99@gmail.com>
2024-08-12 19:12:26 +0530

    home page and entreprenurship page added

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,27 @@
 from flask import render_template
 from . import app
 
-@app.route('/')
+@app.route('/home')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', active_page='home')
+
+@app.route('/academics')
+def academics():
+    return render_template('academics.html',  active_page='academics')
+
+@app.route('/robotics')
+def robotics():
+    return render_template('robotics.html',  active_page='robotics')
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html', )
+
+@app.route('/entrepreneurship')
+def entre():
+    return render_template('entrepreneurship.html',  active_page='entrepreneurship')
+
+@app.route('/teaching')
+def teaching():
+    return render_template('teaching.html',  active_page='teaching')
+
